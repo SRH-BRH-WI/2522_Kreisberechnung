cfea3754940189a7e7d37b72b2ce5e73ca0af689
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-01-10 16:12:13 +0100

    Umgang mit Fließkommazahlen (explizite Konvertierung)

--- src/Kreisberechnung.java
+++ src/Kreisberechnung.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Kreisberechnung {
+    public static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+        System.out.print("Bitte Radius eingeben: ");
+        float radius = eingabe.nextFloat();
+
+        // explizite Typkonvertierung (double -> float)
+        float fläche = 3.1415F * radius * radius;
+        float umfang = 2 * (float)Math.PI * radius;
+
+        System.out.println("Kreisfläche: " + fläche);
+        System.out.println("Umfang:      " + umfang);
+    }
 }
